75bf6f1204d829da4d1c5698400a1680cfeea56b
Mark Atolog <mentalistone@gmail.com>
2022-09-14 16:30:49 +0300

    add possibility to build mods for different platforms

--- ModBuilder.cs
+++ ModBuilder.cs
@@ -1,18 +1,52 @@
-using System;
 using System.IO;
 using System.Linq;
-using UnityEngine;
 using UnityEditor;
+using UnityEngine;
 
 
 public class ModBuilder : MonoBehaviour
 {
-    [MenuItem("Mods/Build Mod")]
-    static void BuildMod()
+    private const string ModsFolderName = "Mods";
+    private readonly static string ModName = Application.productName;
+
+    [MenuItem("Mods/Build for Windows")]
+    public static void ReleaseWindows()
     {
+        BuildMod("windows", BuildTarget.StandaloneWindows64);
+    }
+
+    [MenuItem("Mods/Build for Android")]
+    public static void ReleaseAndroid()
+    {
+        BuildMod("android", BuildTarget.Android);
+    }
+
+    [MenuItem("Mods/Build for iOS")]
+    public static void ReleaseiOS()
+    {
+        BuildMod("iOS", BuildTarget.iOS);
+    }
+
+    [MenuItem("Mods/Build for Linux")]
+    public static void ReleaseLinux()
+    {
+        BuildMod("linux", BuildTarget.StandaloneLinux64);
+    }
+
+    private static void BuildMod(string platform, BuildTarget targetPlatform)
+    {
+        Directory.CreateDirectory(ModsFolderName);
+
         AssetBundleBuild[] buildMap = new AssetBundleBuild[1];
 
-        var assetNames = AssetDatabase.GetAllAssetPaths().Where(s => s.StartsWith("Assets/") && !s.Contains("Editor") && !s.Contains(".meta") && !s.Contains(".unity") && !s.Contains("Scenes")).ToArray();
+        var assetNames = AssetDatabase.GetAllAssetPaths()
+										.Where(s => s.StartsWith("Assets/") && 
+													!s.Contains("Editor") && 
+													!s.Contains(".meta") && 
+													!s.Contains(".unity") && 
+													!s.Contains("Scenes"))
+										.ToArray();
+										
         var addressableNames = new string[assetNames.Length];
 
         for (var i = 0; i < assetNames.Length; i++)
@@ -20,15 +54,24 @@ public class ModBuilder : MonoBehaviour
             addressableNames[i] = assetNames[i].Replace("Assets/", "");
         }
 
-        string modName = Application.productName;
-
-        buildMap[0].assetBundleName = $"{modName}.mod";
+        buildMap[0].assetBundleName = $"{ModName}_{platform}.mod";
         buildMap[0].assetNames = assetNames;
         buildMap[0].addressableNames = addressableNames;
 
-        string modsFolderName = "Mods";
-        Directory.CreateDirectory(modsFolderName);
+        BuildPipeline.BuildAssetBundles(ModsFolderName, buildMap, BuildAssetBundleOptions.None, targetPlatform);
 
-        BuildPipeline.BuildAssetBundles(modsFolderName, buildMap, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+        DeleteRedundantFiles();
+    }
+
+    private static void DeleteRedundantFiles()
+    {
+        var files = Directory.GetFiles(ModsFolderName);
+        foreach (var file in files)
+        {
+            if (!file.EndsWith(".mod"))
+            {
+                File.Delete(file);
+            }
+        }
     }
 }
